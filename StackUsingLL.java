5d471bb94b49b064910f615321e873ce5a122c25
Ritik Kumar <72155695+Ritik2911@users.noreply.github.com>
2022-03-20 16:16:05 +0530

    Add files via upload

---
+++ StackUsingLL.java
@@ -0,0 +1,58 @@
+public class StackUsingLL {
+    static class Node{
+        int data;
+        Node next;
+
+        Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class stack{
+        static Node head = null;
+
+        public boolean isEmpty(Node head){
+            return head == null;
+        }
+
+        public void push(int n){
+            Node newNode = new Node(n);
+            if(isEmpty(head)){
+                head = newNode;
+            }else{
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        public int pop(){
+            if(isEmpty(head)){
+                return -1;
+            }else{
+                int top = head.data;
+                head = head.next;
+                return top;
+            }
+        }
+
+        public int peek(){
+            int top = head.data;
+            return top;
+        }
+    }
+
+    public static void main(String[] args) {
+        stack s = new stack();
+        s.push(3);
+        s.push(6);
+        s.push(53);
+        s.push(34);
+        s.push(8);
+        s.pop();
+        while(s.head != null){
+            System.out.println(s.head.data);
+            s.pop();
+        }
+    }
+}
